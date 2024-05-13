6066f5db88903c14273f9386c6c5533c18054da5
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-05-13 20:26:23 +0900

    add actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Myxogastria0808.github.io",
+  base: "/astro-blog-mytemplate",
   integrations: [react()],
 });
