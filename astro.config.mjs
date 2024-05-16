e48aa5f03d747fbc3e8cc8450dbd29b06f1ed606
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-05-17 02:31:56 +0900

    fix: change title etc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sohosai.github.io",
+  site: "https://Myxogastria0808.github.io",
   base: import.meta.env.PROD ? "/astro-blog-mytemplate" : "/",
   integrations: [react()],
 });
