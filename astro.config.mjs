edf0e8de535c5b13396caba5629f17c64901c2af
Yuki Osada <78744619+Myxogastria0808@users.noreply.github.com>
2025-04-01 13:33:03 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://Myxogastria0808.github.io",
-  base: import.meta.env.PROD ? "/astro-blog-mytemplate" : "/",
+  base: "astro-blog-mytemplate",
   integrations: [react()],
 });
